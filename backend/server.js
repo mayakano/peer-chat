05a862c93e1a8f21b2ebfc03295ee6bada7f7de1
Maya Kano <mayakanoweb@gmail.com>
2022-02-18 02:09:57 -0500

    setting up apiwebserver using express and using dummy data to test

---
+++ backend/server.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const { chats } = require("./data/data");
+const dotenv = require("dotenv");
+//creating instance of that variable
+const app = express();
+dotenv.config();
+
+//creating our express api, initiating a get request to the / route, takes a parameter which is a callback
+//the callback takes request and response. We're going to send a response to the / route.
+app.get("/", (req, res) => {
+  res.send("API is Running Successfully");
+});
+
+app.get("/api/chat", (req, res) => {
+  res.send(chats);
+});
+
+//creating a new endpoint
+app.get("/api/chat/:id", (req, res) => {
+  //   console.log(req.params.id);
+  const singleChat = chats.find((c) => c._id === req.params.id);
+  res.send(singleChat);
+});
+
+//use PORT or if not 5000
+const PORT = process.env.PORT || 5000;
+
+//now with app variable we can start our server
+app.listen(PORT, console.log(`Server Started on ${PORT}`));
+
+//to run file in terminal write npm start
+//everytime we write something on this file we have to restart our server.
